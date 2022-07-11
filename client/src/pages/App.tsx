74cf05f89ed11d6253288d4418af685f4010ed5e
zahinafsar <afsarzahin@gmail.com>
2022-07-11 23:31:21 +0600

    FE: Added Necessary Modules

---
+++ client/src/pages/App.tsx
@@ -0,0 +1,53 @@
+import { Sidenav, Nav, Container, Content, Header, Navbar, Sidebar, CustomProvider } from 'rsuite';
+import { Dashboard } from '@rsuite/icons';
+import Home from './home';
+
+
+const headerStyles = {
+    padding: 18,
+    fontSize: 16,
+    height: 56,
+    background: '#34c3ff',
+    color: ' #fff',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden'
+};
+
+function App() {
+    return (
+        <CustomProvider>
+            <div className="show-fake-browser sidebar-page">
+                <Container>
+                    <Sidebar
+                        style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}
+                        width={260}
+                        collapsible
+                    >
+                        <Sidenav.Header>
+                            <div style={headerStyles}>
+                                <span style={{ marginLeft: 12 }}> BRAND</span>
+                            </div>
+                        </Sidenav.Header>
+                        <Sidenav style={{ height: '100%' }}>
+                            <Sidenav.Body>
+                                <Nav>
+                                    <Nav.Item eventKey="1" active icon={<Dashboard />}>
+                                        Dashboard
+                                    </Nav.Item>
+                                    <Nav.Item eventKey="2">
+                                        User Group
+                                    </Nav.Item>
+                                </Nav>
+                            </Sidenav.Body>
+                        </Sidenav>
+                    </Sidebar>
+                    <Container>
+                        <Home />
+                    </Container>
+                </Container>
+            </div>
+        </CustomProvider>
+    )
+}
+
+export default App
---
+++ client/src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import './home.scss'
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Hello React!</h1>
+    </div>
+  )
+}
+
+export default Home
